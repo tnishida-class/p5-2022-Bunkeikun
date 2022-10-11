21a49651166ae61e5a2f9038a73b9fdb2de26bc8
Bunkeikun <carbuncle0617@gmail.com>
2022-10-11 12:15:11 +0900

    update1

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,19 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if((i + j) % 2 == 1){
+        fill(128)
+        rect(size * i, size * j, size, size);// BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+        if(j < 3){
+          fill(255,0,0)
+          ellipse(size * i + 12.5, size * j + 12.5, 20)
+        }
+        if(j > 4){
+          fill(0)
+          ellipse(size * i + 12.5, size * j + 12.5, 20)
+        }
     }
+    
   }
+}
 }
