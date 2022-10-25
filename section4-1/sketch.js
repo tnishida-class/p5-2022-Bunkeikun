66ed3151b64a37d7abd922405316b5f23e887ee6
Bunkeikun <carbuncle0617@gmail.com>
2022-10-25 11:13:44 +0900

    change1
    required

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -20,15 +20,18 @@ function setup(){
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
   // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
+  average = sum / 10
 
   largest = 0;
   for(let i = 0; i < scores.length; i++){
     // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+  if(scores[i] > largest){largest = scores[i]}
   }
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
     // BLANK[3]　ヒント：最小値とだいたい同じ
+    if(scores[i] < smallest){smallest = scores[i]}
   }
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
@@ -41,10 +44,21 @@ function setup(){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
     // BLANK[4] ヒント: 条件分岐を使って色を変更します
-    rect(i * dx + 2, height - h, dx - 4, h);
     fill(0);
+    if(scores[i] == largest){fill(255,0,0)}
+    if(scores[i] == smallest){fill(0,0,255)}
+    rect(i * dx + 2, height - h, dx - 4, h);
     text(scores[i].toPrecision(3), i * dx, height - h);
   }
 
   // BLANK[5] 平均点の線を引きます
+  const avgh = height * average / 100
+  fill(0,255,0);
+  var context = canvas.getContext('2d');
+  context.beginPath();
+  context.moveTo(0, height - avgh);
+  context.lineTo(400, height - avgh);
+  context.strokeStyle = '#00FF00' ;
+  context.stroke();
+  text(average.toPrecision(3), 0, height - avgh);
 }
